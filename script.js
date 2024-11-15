378adc8b37bf823a978a6702fbec0e0fafabd02f
Aayushrohilla <aayushrohilla3@gmail.com>
2024-11-15 14:38:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	let str= input
+	let freqobj= {}
+	for(let i= 0; i < str.length; i++)
+		{
+			if(!freqobj[str.chartAt(i)])
+			{
+				freqobj[str.charAt(i)] = 1
+			}
+			else
+			{
+				freqobj[str.charAt(i) = freqobj[str.charAt(i)]] + 1
+			}
+		}
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+
